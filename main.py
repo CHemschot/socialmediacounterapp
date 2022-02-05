c8523a5d99f51636889a1b470f1825d792d29346
CHemschot <98435281+CHemschot@users.noreply.github.com>
2022-02-05 15:55:43 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,41 @@
+# Exercise 1
+
+#animal = 'cat'
+#vegetable = 'brocoli'
+#mineral = 'gold'
+
+#print('Here is an animal, a vegetable, and a mineral.')
+
+#print(animal)
+#print(vegetable)
+#print(mineral)
+
+#Exercise 2
+
+#something = input('Please type something and press enter:')
+#print('You entered:')
+#print(something)
+
+#Exercise 3
+
+text = input('What would you like your cat to say? ')
+text_length = len(text)
+
+print('           {}'.format('_'*text_length))
+print('         < {} >'.format(text))
+print('           {}'.format('-'*text_length))
+print('         /')
+print('  //_// /')
+print('( o.o )')
+print(' > ^ <')
+
+
+
+
+
+
+
+
+
+
+
